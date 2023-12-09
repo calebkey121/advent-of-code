27b9011f8a8c5145e9b21e9e2a528bb8e030d418
Caleb Key <calebkey121@gmail.com>
2023-12-08 23:25:47 -0600

    part1

--- day9/day9_part1.py
+++ day9/day9_part1.py
@@ -1,10 +1,37 @@
 import time
 
 def main():
-    input_file = "input.txt"
+    input_file = "day9_input.txt"
 
     with open(input_file) as f:
         lines = f.readlines()
+
+        total = 0
+        for line in lines:
+            oasis = []
+            oasis.append([ int(val) for val in line.split() ])
+
+            allZeros = False
+            i = 0
+            while not allZeros:
+                diffs = []
+                allZeros = True
+                for j in range(len(oasis[i]) - 1):
+                    diff = oasis[i][j + 1] - oasis[i][j]
+                    if diff != 0:
+                        allZeros = False
+                    diffs.append(diff)
+                oasis.append(diffs)
+                i += 1
+
+            oasis = oasis[::-1]
+            for level in range(len(oasis) - 1):
+                oasis[level + 1].append(oasis[level + 1][-1] + oasis[level][-1])
+
+            total += oasis[-1][-1]
+        print(total)
+            
+
 
 if __name__ == "__main__":
     start_time = time.time()
