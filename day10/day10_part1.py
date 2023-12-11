526ea8b1691ecdf552dd37aa6df9ee92ae33b94b
Caleb Key <calebkey121@gmail.com>
2023-12-11 09:33:34 -0600

    part1

--- day10/day10_part1.py
+++ day10/day10_part1.py
@@ -1,58 +1,86 @@
 import time
 
-pipeLen = 0
+mapLen = 0
+mapData = []
 
-class Pipe():
-    def __init__(self, pipeType, row, col, visited=False) -> None:
-        self.pipeType = pipeType
-        self.row = row
-        self.col = col
-        self.visited = visited
-        self.north = self.south = self.east = self.west = None
+fromSouthPipes = ('|', '7', 'F') # possible pipes to travel to if your are coming from sout
+fromNorthPipes = ('|', 'L', 'J')
+fromWestPipes = ('-', 'J', '7')
+fromEastPipes = ('-', 'L', 'F')
 
-        if self.row > 0:
-            self.north = (self.row - 1, self.col)
-        if self.row < pipeLen - 1:
-            self.south = (self.row + 1, self.col)
-        if self.col > 0:
-            self.east = (self.row, self.col - 1)
-        if self.col < pipeLen - 1:
-            self.west = (self.row, self.col + 1)
+def findNextPipe(loop):
+    row = loop[-1][0]
+    col = loop[-1][1]
+    # Starting from Start
+    if len(loop) == 1:
+        # if you are coming from a starting position, you have to check all four direction, where there should be two ways
+        # Going North (from South)
+        if row > 0:
+            checkPipes = fromSouthPipes
+            direction = (row - 1, col)
+            pipe = mapData[direction[0]][direction[1]]
+            if (len(loop) == 1  or direction != loop[-2]) and pipe in checkPipes:
+                return direction
+            
+        # Going South (from North)
+        if row < mapLen - 1:
+            checkPipes = fromNorthPipes
+            direction = (row + 1, col)
+            pipe = mapData[direction[0]][direction[1]]
+            if (len(loop) == 1  or direction != loop[-2]) and pipe in checkPipes:
+                return direction
 
-    def __repr__(self) -> str:
-        return self.pipeType
-    
+        # Going East (from West) 
+        if col < mapLen - 1:
+            checkPipes = fromWestPipes
+            direction = (row, col + 1)
+            pipe = mapData[direction[0]][direction[1]]
+            if (len(loop) == 1  or direction != loop[-2]) and pipe in checkPipes:
+                return direction
 
-def findNextPipe(pipeMap, pipe):
-    northPipes = ('|', '7', 'F', 'S') # more like 'from south pipes' but lets simplify to if youre going north pipes
-    if pipe.north:
-        path = pipeMap[pipe.north[0]][pipe.north[1]]
-        if not path.visited and path.pipeType in northPipes:
-            path.visited = True
-            return path
-        
-    southPipes = ('|', 'L', 'J', 'S')
-    if pipe.south:
-        path = pipeMap[pipe.south[0]][pipe.south[1]]
-        if not path.visited and path.pipeType in southPipes:
-            path.visited = True
-            return path
-        
-    eastPipes = ('-', 'L', 'F', 'S')
-    if pipe.east:
-        path = pipeMap[pipe.east[0]][pipe.east[1]]
-        if not path.visited and path.pipeType in eastPipes:
-            path.visited = True
-            return path
-        
-    westPipes = ('-', 'J', '7', 'S')
-    if pipe.west:
-        path = pipeMap[pipe.west[0]][pipe.west[1]]
-        if not path.visited and path.pipeType in westPipes:
-            path.visited = True
-            return path
-        
-    raise RuntimeError("Should never not find one")
+        # Going West (from East)      
+        if col > 0:
+            checkPipes = fromEastPipes
+            direction = (row, col - 1)
+            pipe = mapData[direction[0]][direction[1]]
+            if (len(loop) == 1  or direction != loop[-2]) and pipe in checkPipes:
+                return direction
+    else:
+        # Just return the direction your letter is pointing
+        dx = loop[-2][1] - loop[-1][1]
+        dy = loop[-2][0] - loop[-1][0]
+        pipe = mapData[row][col]
+
+        north = (row - 1, col)
+        south = (row + 1, col)
+        east = (row, col + 1)
+        west = (row, col - 1)
+        if dy == -1:
+            # coming from North
+            if pipe == '|': return south
+            elif pipe == 'J': return west
+            elif pipe == 'L': return east
+            else: raise RuntimeError("Not a valid pipe")
+        elif dy == 1:
+            # coming from South
+            if pipe == '|': return north
+            elif pipe == 'F': return east
+            elif pipe == '7': return west
+            else: raise RuntimeError("Not a valid pipe")
+        elif dx == 1:
+            # coming from East
+            if pipe == '-': return west
+            elif pipe == 'L': return north
+            elif pipe == 'F': return south
+            else: raise RuntimeError("Not a valid pipe")
+        elif dx == -1:
+            # coming from West
+            if pipe == '-': return east
+            elif pipe == 'J': return north
+            elif pipe == '7': return south
+            else: raise RuntimeError("Not a valid pipe")
+
+    raise RuntimeError("Should have returned something")
 
 
     
@@ -63,33 +91,28 @@ def main():
 
     with open(input_file) as f:
 
+        global mapLen, mapData
         mapData = [ line.rstrip('\n') for line in f.readlines() ]
-        pipeMap = []
+        mapLen = len(mapData)
 
         loop = []
-        global pipeLen 
-        pipeLen = len(mapData)
         
-        for row in range(len(mapData)):
-            pipeMap.append([])
-            for col in range(len(mapData[row])):
+        start = None
+        for row in range(mapLen):
+            for col in range(mapLen):
                 if mapData[row][col] == 'S':
-                    start = (Pipe('S', row, col, visited=True))
+                    start = (row, col)
                     loop.append(start)
-                    pipeMap[row].append(start)
-                else:
-                    pipeMap[row].append(Pipe(mapData[row][col], row, col))
-
-        # Start finding the mainloop
-        nextPipe = findNextPipe(pipeMap, loop[-1])
-        loop.append(nextPipe)
-        nextPipe = findNextPipe(pipeMap, loop[-1])
-        loop.append(nextPipe)
-        start.visited = False
-        while loop[-1] != start:
-            nextPipe = findNextPipe(pipeMap, loop[-1])
-            loop.append(nextPipe)
+                    break
+            if start: break
             
+        nextPipe = findNextPipe(loop)
+        loop.append(nextPipe)
+
+        while loop[-1] != start:
+            nextPipe = findNextPipe(loop)
+            loop.append(nextPipe)
+
         print(len(loop) // 2)
 
 
