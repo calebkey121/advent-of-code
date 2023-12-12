4b894723404dafde0fe91144bbd4f3a8098e292e
Caleb Key <calebkey121@gmail.com>
2023-12-11 22:26:39 -0600

    expanded

--- day11/day11_part1.py
+++ day11/day11_part1.py
@@ -4,7 +4,37 @@ def main():
     input_file = "input.txt"
 
     with open(input_file) as f:
-        lines = f.readlines()
+        lines = [ line.rstrip() for line in f.readlines() ]
+
+        emptyRows = []
+        for row in range(len(lines)):
+            if lines[row] == '.' * len(lines[row]):
+                emptyRows.append(row)
+
+        emptyCols = []
+        for col in range(len(lines[0])):
+            dots = True
+            for row in lines:
+                if row[col] != '.':
+                    dots = False
+                    break
+            if dots:
+                emptyCols.append(col)
+
+        expanded = []
+        for row in range(len(lines)):
+            expandedRow = ""
+            prevCol = 0
+            for col in emptyCols:
+                expandedRow += lines[row][prevCol:col] + '..'
+                prevCol = col + 1
+            expandedRow += lines[row][prevCol:]
+            expanded.append(expandedRow)
+
+        for row in emptyRows[::-1]:
+            expanded.insert(row, '.' * len(expanded[0]))
+
+        # Expandings Finished
 
 if __name__ == "__main__":
     start_time = time.time()
