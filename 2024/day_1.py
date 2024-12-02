566a83c85c4db973eb157c123b43a2df55d9bb30
Caleb Key <calebkey121@gmail.com>
2024-12-02 09:43:08 -0600

    day1 part 2

--- 2024/day_1.py
+++ 2024/day_1.py
@@ -1,6 +1,6 @@
 import time
 
-def main():
+def star_1():
     input_file = "2024/input/day_1_input.txt"
 
     with open(input_file) as f:
@@ -17,6 +17,30 @@ def main():
         print(f"{l} - {r} = {l - r}")
         total += abs(l - r)
     print(total)
+
+def main():
+    input_file = "2024/input/day_1_input.txt"
+
+    with open(input_file) as f:
+        lines = f.readlines()
+        left_list, right_list = [], []
+        for line in lines:
+            left, right = line.split()
+            left_list.append(int(left))
+            right_list.append(int(right))
+
+    similarity_score = {}
+    for l in left_list:
+        similarity_score[l] = 0
+    
+    for r in right_list:
+        if r in similarity_score:
+            similarity_score[r] += 1
+    
+    score = 0
+    for l in left_list:
+        score += l * similarity_score[l]
+    print(score)
 
 
 if __name__ == "__main__":
